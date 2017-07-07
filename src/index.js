a936bf60a1b9dc0294e3edb1dde3b6f04355f568
jessica <samandjesslovepie@gmail.com>
2017-07-07 14:37:34 -0600

    mini project done

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import { Provider } from "react-redux";
 
 import store from './store';
 
+
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </BrowserRouter>, 
+    <BrowserRouter>
+      <Provider store={ store }>
+        <App />
+      </Provider>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 registerServiceWorker();
